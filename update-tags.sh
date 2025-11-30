6e255ae19dd2ed1e334195877641fe703cc40861
sarah-strawberries <blue.howrse@gmail.com>
2025-11-29 23:49:58 -0700

    add script for updating tags; change images to v0.0.4

---
+++ update-tags.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NEW_TAG="0.0.4"
+
+# Find and replace all occurrences of sarahstrawberries/secretarai-api and sarahstrawberries/secretarai-web tags
+find . -type f \( -name "*.yaml" -o -name "*.yml" -o -name "Dockerfile" \) \
+  -exec sed -i -E "s|(sarahstrawberries/secretarai-api):[0-9]+\.[0-9]+\.[0-9]+|\1:${NEW_TAG}|g" {} \; \
+  -exec sed -i -E "s|(sarahstrawberries/secretarai-web):[0-9]+\.[0-9]+\.[0-9]+|\1:${NEW_TAG}|g" {} \;
+
+echo "Updated all image tags to ${NEW_TAG}"
