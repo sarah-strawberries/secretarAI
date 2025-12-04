9cbb45168205fd37a3d7d9eeb69f70fd27c88186
sarah-strawberries <blue.howrse@gmail.com>
2025-12-04 09:12:59 -0700

    try to upgrade tailwind css to v4.1.16 for simplified color vars; results in getting stuck on transforming step in build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     // Dev proxy to avoid CORS while developing locally. Requests to /api/* will be
     // forwarded to the API running on localhost:5000.
